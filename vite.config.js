fbacd4c5c42172d2945cc2f871e855e9f94d4663
Dmytro Popsha <popsha.dima@gmail.com>
2025-02-16 18:29:35 +0200

    step2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: true,
+    assetsInlineLimit: 0, 
+  },
+  optimizeDeps: {
+    include: ["video.js"], 
+  },
+  css: {
+    modules: {
+      localsConvention: "dashes",
+    },
+  },
+});
